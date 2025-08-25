d486bbd737a8ac36485854c19131aecbe464866d
Andreaedx <104171786+Andreaedx@users.noreply.github.com>
2025-08-25 13:25:44 -0700

    created the computerChoice function, HumanChoice function and the playRound function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock-Paper-Scissor-game
+Rock Paper Scissor Game played in the console.
+it will be played entirely against the cpmputer.
---
+++ RPSG.js
@@ -0,0 +1,70 @@
+const getComputerChoice = function(){
+    //random generation of 0, 1 and 2
+   let rand = Math.floor(Math.random() * 3);
+   switch(rand){
+    case 0:
+        return "rock";
+    case 1:
+        return "paper";
+    case 2:
+        return "scissor";    
+   }
+} 
+
+const getHumanChoice = function(){
+    return prompt("choice: ").toLowerCase();
+}
+
+let computerScore = 0;
+let playerScore = 0;
+
+const playRound = function(computerChoice, humanChoice){
+    console.log(computerChoice);
+    console.log(humanChoice);
+
+    //check for ties
+    if(humanChoice === computerChoice){
+        return "it's a tie!";
+    }
+    //check for computer wins
+    if(computerChoice === "rock"){
+        if(humanChoice === "scissor"){
+            computerScore++
+            return "you Lose! rock beats scissor";
+        }
+    }
+    if(computerChoice === "paper"){
+        if(humanChoice === "rock"){
+            computerScore++
+            return "you Lose! paper beats rock";
+        }
+    }
+    if(computerChoice === "scissor"){
+        if(humanChoice === "paper"){
+            computerScore++
+            return "you Lose! scissor beats paper";
+        }
+    }
+
+    //check for human wins
+    if(humanChoice === "rock"){
+        if(computerChoice === "scissor"){
+            playerScore++
+            return "you win! rock beats paper";
+        } 
+    }
+    if(humanChoice === "paper"){
+        if(computerChoice === "rock"){
+            playerScore++
+            return "you win! paper beats rock";
+        }
+    }
+    if(humanChoice === "scissor"){
+        if(computerChoice === "paper"){
+            playerScore++
+            return "you win! scissor beats paper";
+        }
+    }
+}
+console.log(playRound(getComputerChoice(), getHumanChoice()));
+
